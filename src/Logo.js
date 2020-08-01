1903b745e9a07fb687708222be037824975cee11
Aras Sulaiman <arasko87@gmail.com>
2020-08-01 10:54:53 +0100

    Ex 1 inClass Done

--- src/Logo.js
+++ src/Logo.js
@@ -3,13 +3,19 @@ import React from "react";
 const Logo = (props) =>{
         // we have passed line 5 to App.j and we deleted from here
     // const appName = "Pokedex";
-    console.log(props)
+   
+    function logWhenClicked(){
+    return console.log("This is a new message is a test of the onClick ");
+    }
+
     return(
     <header>
       <h1> Welcome to the {props.appName} </h1>
-      <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYvLffuKqbWPk0NgQpFvlfDynzU9cS1sZYZw&usqp=CAU" alt ="pikachu"></img>
+      <img onClick={logWhenClicked} src ="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYvLffuKqbWPk0NgQpFvlfDynzU9cS1sZYZw&usqp=CAU" alt ="pikachu" ></img>
       </header>
     )
   }
+
+
 
   export default Logo;
